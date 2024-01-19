924d1fdc20946b51d2aa90245445df40a228fa2d
Homelander-69 <abhinashpaul370@gmail.com>
2024-01-20 02:30:44 +0600

    update

--- config/dbConfig.js
+++ config/dbConfig.js
@@ -1,9 +1,11 @@
 const mongoose = require("mongoose");
 
+let { USER_NAME, USER_PASSWORD, PROJECT_NAME } = process.env;
+
 function dbConnection() {
    mongoose
       .connect(
-         "mongodb+srv://abhinashpaul:aditi017@cluster0.bo5wmo1.mongodb.net/E-commerce?retryWrites=true&w=majority"
+         `mongodb+srv://${USER_NAME}:${USER_PASSWORD}@cluster0.bo5wmo1.mongodb.net/${PROJECT_NAME}?retryWrites=true&w=majority`
       )
       .then(() => console.log("Database-Connected!"));
 }
--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require("express");
+require("dotenv").config();
+
 const app = express();
 const Task = require("./model/taskSchema.js");
 const dbConnection = require("./config/dbConfig.js");
 
 app.use(express.json());
-
 dbConnection();
 
 app.listen(8000, function () {
